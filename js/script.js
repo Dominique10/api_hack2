77b273e4d2158a2f859a20814fc25d5302c70d9b
cssdominique <dominique@christiansteven.com>
2016-06-09 17:51:23 -0400

    Created getComicCover

--- js/script.js
+++ js/script.js
@@ -2,21 +2,14 @@ var userSelection;
 var idNumber=0;
 var comicsInformation;
 var input;
+var comicCover;
 var params={
 			apikey: '9dcb20f1185fa9fc6ead79c1ce2be593'
 			
 		};
 
 $(document).ready(function(){
-	//getMarvelHero(input);
-	
-		// url2= "http://gateway.marvel.com/v1/public/characters/"+idNumber+"/comics?";
-		// $.getJSON(url2,params,function(data2){
-		// 	console.log("Title "+data2.data.results[0].title);
-		// 	console.log("Description "+data2.data.results[0].description);
-		// 	console.log("resourceURL "+data2.data.results[0].resourceURL);
-		// 	console.log("resourceURL "+data2.data.results[0].urls[0].url);
-		// });
+
 });
 
 $('#myForm').submit(function(e){
@@ -25,21 +18,17 @@ $('#myForm').submit(function(e){
 	console.log(input);
 	getMarvelHero(input);
 })
-//http://gateway.marvel.com:80/v1/public/characters?name=daredevil&apikey=9dcb20f1185fa9fc6ead79c1ce2be593
 
 function getMarvelHero(hero){
 	url = "http://gateway.marvel.com/v1/public/characters?name="+hero;
 	$.getJSON(url,params,function(data){
-		//console.log(data.data);
+
 		console.log(data.data.results[0]);
 		console.log(data.data.results[0].id);
 		idNumber = data.data.results[0].id;
 
 		console.log(data.data.results[0].comics.items[0]);
-		// comics = data.data.results[0].comics.items;
-		// for (var i =0,i < comics.length - 1; i++) {
-		// 	comics[i]
-		// }
+
 		getComics(idNumber);
 	});
 }
@@ -49,16 +38,35 @@ function getComics(idNumber){
 	$.getJSON(url2,params,function(data2){
 		 console.log("Title "+data2.data.results[0].title);
 		 console.log(data2.data.results);
-		// console.log("Description "+data2.data.results[0].description);
-		// console.log("resourceURL "+data2.data.results[0].resourceURL);
-		// console.log("resourceURL "+data2.data.results[0].urls[0].url);
+
 		comicsInformation = data2.data.results;
 		for (var i =0; i < comicsInformation.length; i++) {
-			//data2.data.results[i];
+
 			console.log("Title "+comicsInformation[i].title);
 			console.log("Description "+comicsInformation[i].description);
-			//console.log("resourceURL "+data2.data.results[i].resourceURL);
+
 			console.log("resourceURL "+comicsInformation[i].urls[0].url);
+			comicCover = getComicCover(comicsInformation[i].id);
+
+			var result = $('.templates .title').clone();
+	
+			// Set the title properties in result
+			var titleElem = result.find('.title-text a');
+			titleElem.attr('href', comicsInformation[i].urls[0].url);
+			titleElem.text(comicsInformation[i].title);
+
+			var descriptionElem = result.find('.description');
+			descriptionElem.text(comicsInformation[i].description);
+
+			var coverElem = result.find('.cover img');
+			coverElem.attr('src', comicCover);
 		};
+	});
+}
+
+function getComicCover(comicCode){
+	url3= "http://gateway.marvel.com/v1/public/comics/"+comicCode;
+	$.getJSON(url3,params,function(data3){
+		return data3.data.results[0].images[0].path +".jpg";
 	});
 }
