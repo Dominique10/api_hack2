ee8621e5ea007195ca992cec0c49fe86e03523e7
cssdominique <dominique@christiansteven.com>
2016-06-09 10:55:52 -0400

    Connecting to api

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	//$.get( "http://gateway.marvel.com:80/v1/public/characters?name=daredevil&apikey=9dcb20f1185fa9fc6ead79c1ce2be593", function( data ) {
+  //$( ".result" ).html( data );
+ // console.log(data);
+  //alert( "Load was performed." );
+
+
+
+
+	var params={
+			//part: 'snippet',
+			//name: 'daredevil',
+			apikey: '9dcb20f1185fa9fc6ead79c1ce2be593'
+			
+		};
+		url = "http://gateway.marvel.com/v1/public/characters?name=daredevil";
+		$.getJSON(url,params,function(data){
+			console.log(data.data);
+			//showSearchResults(data.items);
+		});
+});
+
+//http://gateway.marvel.com:80/v1/public/characters?name=daredevil&apikey=9dcb20f1185fa9fc6ead79c1ce2be593
